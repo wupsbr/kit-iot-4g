b453961b3841a43671995c85f6d3b7f04d781c60
Vitor Leal <vitorleal1@gmail.com>
2014-07-24 20:33:37 -0300

    generated the bin file

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,8 @@
 var kitiot  = require('../lib/kit-iot'),
     Insight = require('insight'),
     pkg     = require('../package.json'),
+    token   = require('../lib/token'),
+
     argv    = require('minimist')(process.argv.slice(2)),
     insight = new Insight({
         trackingCode  : 'UA-5427757-50',
@@ -12,10 +14,41 @@ var kitiot  = require('../lib/kit-iot'),
     args    = process.argv,
     isConsole = false;
 
-//Initiate the kit
-if (args[2] && args[2] === '-c') {
+//Check if we want to use the console mode ...
+isConsole = argv.setup || argv.console;
+
+//Check for console setup ...
+if (argv.setup) {
   isConsole = true;
+  if (!argv.name || !argv.username || !argv.password || !argv.email || !argv.tel || !argv.apikey || !argv.token)
+  {
+    console.log("Erro! os seguintes parametros sao necessarios:");
+    console.log("  --name");
+    console.log("  --password");
+    console.log("  --email");
+    console.log("  --tel");
+    console.log("  --apikey");
+    console.log("  --token");
+    console.log("Exemplo:");
+    console.log("kit-iot-4g --setup --name=Seu Nome --username=cprecifeA_b --password=0123 --email=a@a.com --tel=123 --apikey=a012b024 --token=0123");
+    return ;
+  } else {
+    t.saveConfig({
+      "name":     argv.name,
+      "usuario":  argv.username,
+      "password": argv.password,
+      "email":    argv.email,
+      "tel":      argv.tel,
+      "apikey":   argv.apikey,
+      "token":    argv.token
+    });
+  }
 }
+
+// //Initiate the kit
+// if (args[2] && args[2] === '-c') {
+//   isConsole = true;
+// }
 
 var KitIoT = new kitiot(isConsole);
 insight.track('init');
